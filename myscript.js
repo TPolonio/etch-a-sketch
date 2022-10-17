84e222c62b86336466c5d351d6003e85647de7e3
TPolonio <tpolonio@live.com.pt>
2022-10-17 23:39:21 +0100

    Finished project

--- myscript.js
+++ myscript.js
@@ -1,16 +1,42 @@
-const container =  document.querySelector('.container')
+const container =  document.querySelector('.container');
+const button = document.querySelector('.button');
 
 //For loop to add squares:
-
+document.documentElement.style.setProperty("--columns-row", 16);
 for (let i = 0; i < 256; i++) {
-    const square = document.createElement('div');
+    let square = document.createElement('div');
     square.classList.add('square');
     container.appendChild(square);
 }
 
-const squares = document.querySelectorAll('.square')
 
-squares.forEach(square => {
+squares = document.querySelectorAll('.square')
+hover();
+
+
+
+button.addEventListener('click',(e)=>{
+    let gridSize = prompt('Number of squares per column?' , '16');
+        resetGrid();
+        newGrid(gridSize);
+            squares.forEach(square => {
+            square.addEventListener('mouseover', (e) =>{
+            square.style.backgroundColor = 'crimson'
+        });
+
+        square.addEventListener('mouseout', (e) =>{
+        setTimeout(function(){ 
+            square.style.backgroundColor = 'white'
+        },1000)
+            });
+});
+    } 
+,)
+
+
+
+function hover() {
+    squares.forEach(square => {
     square.addEventListener('mouseover', (e) =>{
         square.style.backgroundColor = 'crimson'
         });
@@ -21,10 +47,27 @@ squares.forEach(square => {
         },1000)
             });
 });
+}
 
 
+function resetGrid () {
+    for (let i = container.childNodes.length - 1; i >= 0; i--) {
+        container.removeChild(container.childNodes[i]);
+         }
+}
 
+function newGrid(gridSize) {
+    document.documentElement.style.setProperty("--columns-row", gridSize);
+    for (let i = 0; i < Math.pow(gridSize, 2); i++) {
+        square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
+        squares = document.querySelectorAll('.square')
+        hover();
 
+    }
+
+}
     
 
 
